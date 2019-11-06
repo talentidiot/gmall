4fb2d829ff5c2e9769789dc55d57839d64e6ed08
wanghongda <541588991@qq.com>
2019-11-06 17:47:23 +0800

    新建user项目

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.atguigu.gmall.user.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
